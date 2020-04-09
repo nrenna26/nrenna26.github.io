c20d91604a1b2c02b1e866a8007e6cb9aa339b0b
nrenna26 <60246805+nrenna26@users.noreply.github.com>
2020-04-09 16:46:56 -0700

    ye

--- interactive/main.js
+++ interactive/main.js
@@ -3,15 +3,69 @@ $(document).ready(function(){
     $(".title").fadeOut();
     $(".first").fadeIn();
   });
+
+  $("#button_1").click(function(){
+      $(".first").fadeOut();
+      $(".second").fadeIn();
+    });
+
+  $("#button_2").click(function(){
+      $(".second").fadeOut();
+      $(".third").fadeIn();
+    });
+
+  $("#button_3").click(function(){
+      $(".third").fadeOut();
+      $(".fourth").fadeIn();
+    });
+
+  $("#button_4").click(function(){
+      $(".fourth").fadeOut();
+      $(".fifth").fadeIn();
+    });
+
+  $("#button_5").click(function(){
+      $(".fifth").fadeOut();
+      $(".sixth").fadeIn();
+    });
+
+  $("#button_6").click(function(){
+      $(".sixth").fadeOut();
+      $(".seventh").fadeIn();
+    });
+
+  $("#button_7").click(function(){
+      $(".seventh").fadeOut();
+      $(".eigth").fadeIn();
+    });
+
+  $("#button_8").click(function(){
+      $(".eigth").fadeOut();
+      $(".ninth").fadeIn();
+    });
+
+  $("#button_9").click(function(){
+      $(".ninth").fadeOut();
+      $(".tenth").fadeIn();
+    });
+
+  $("#button_10").click(function(){
+      $(".tenth").fadeOut();
+      $(".eleventh").fadeIn();
+    });
+
+  $("#button_11").click(function(){
+      $(".eleventh").fadeOut();
+      $(".twelfth").fadeIn();
+    });
+
+  $("#button_12").click(function(){
+      $(".twelfth").fadeOut();
+      $(".thirteenth").fadeIn();
+    });
+
+  $("#button_13").click(function(){
+      $(".thirteenth").fadeOut();
+      $(".title").fadeIn();
+    });
 });
-// $(document).ready(function(){
-//   $("#button1").click(function(){
-//     $(".first").fadeOut();
-//   });
-// });
-//
-// $(document).ready(function(){
-//   $("#button1").click(function(){
-//     $(".second").fadeIn();
-//   });
-// });
