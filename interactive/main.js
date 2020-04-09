fae197a6fadc76d7b7269693b5da09f6146c6ce5
nrenna26 <60246805+nrenna26@users.noreply.github.com>
2020-04-09 14:37:01 -0700

    y

---
+++ interactive/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $("#button").click(function(){
+    $(".title").fadeOut();
+  });
+});
+
+$(document).ready(function(){
+  $("#button").click(function(){
+    $(".first").fadeIn();
+  });
+});
+$(document).ready(function(){
+  $("#button1").click(function(){
+    $(".first").fadeOut();
+  });
+});
+
+$(document).ready(function(){
+  $("#button1").click(function(){
+    $(".second").fadeIn();
+  });
+});
